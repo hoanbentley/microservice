528b63ba9b712c8ae84aeba5c1368ea4f1a45a34
hoantk <hoan.truong@timo.vn>
2023-02-01 15:34:54 +0700

    init some modules like notification and amqp

---
+++ notification/src/main/java/com/tony/notification/NotificationRepository.java
@@ -0,0 +1,6 @@
+package com.tony.notification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+}
